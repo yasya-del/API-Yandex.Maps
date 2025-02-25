6f1cfbfa2a54b84a8bd49a335d3f9be3557371be
вася <vasiliszaitseva@yandex.ru>
2025-02-25 18:09:01 +0300

    часть 2(вроде работает, но у меня почему то иногда виснет)

--- main.py
+++ main.py
@@ -4,37 +4,57 @@ import sys
 import pygame
 import requests
 
-x = float(input('Введите координаты объекта: '))
-y = float(input())
-s = float(input('Введите масштаб карты: '))
-server_address = 'https://static-maps.yandex.ru/v1?'
-api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
-ll_spn = f'll={x},{y}&spn={s},{s}'
-# Готовим запрос.
 
-map_request = f"{server_address}{ll_spn}&apikey={api_key}"
-response = requests.get(map_request)
+def get_image(s1, s2):
+    server_address = 'https://static-maps.yandex.ru/v1?'
+    api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
+    ll_spn = f'll={x},{y}&spn={s1},{s2}'
+    map_request = f"{server_address}{ll_spn}&apikey={api_key}"
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    return map_file
 
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
-# Запишем полученное изображение в файл.
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
-
-# Инициализируем pygame
+x, y = map(float, input('Введите координаты объекта: ').split(','))
+s1, s2 = map(float, input('Введите масштаб карты: ').split(','))
+map_file = get_image(s1, s2)
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
+FPS = 60
+clock = pygame.time.Clock()
 # Рисуем картинку, загружаемую из только что созданного файла.
 screen.blit(pygame.image.load(map_file), (0, 0))
 # Переключаем экран и ждем закрытия окна.
 pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
+run = True
+while run:
+    for event in pygame.event.get():
+        if pygame.event.wait().type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEUP:
+            if s1 <= 20.75:
+                s1 += 0.25
+            if s2 <= 20.75:
+                s2 += 0.25
+            print(s1, s2)
+            map_file = get_image(s1, s2)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEDOWN:
+            print(1)
+            if s1 >= 0.252:
+                s1 -= 0.25
+            if s2 >= 0.252:
+                s2 -= 0.25
+            print(s1, s2)
+            map_file = get_image(s1, s2)
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+    clock.tick(FPS)
 pygame.quit()
 
 # Удаляем за собой файл с изображением.
