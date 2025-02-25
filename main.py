8390598d94bfe155c61881769662b924664f5f09
вася <vasiliszaitseva@yandex.ru>
2025-02-25 17:29:04 +0300

    часть 1 решена

--- main.py
+++ main.py
@@ -1,51 +1,53 @@
-import pygame
+import os
 import sys
-from io import BytesIO  # Этот класс поможет нам сделать картинку из потока байт
 
 import requests
-from PIL import Image
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel
 
-from search_delta import find_delta
+SCREEN_SIZE = [600, 450]
 
-# Пусть наше приложение предполагает запуск:
-# python search.py Москва, ул. Ак. Королева, 12
-# Тогда запрос к геокодеру формируется следующим образом:
-toponym_to_find = ",".join(sys.argv[1:])
+x = float(input())
+y = float(input())
 
-geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
-geocoder_params = {
-    "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
-    "geocode": toponym_to_find,
-    "format": "json"}
+class Example(QWidget):
+    def init(self):
+        super().init()
+        self.getImage()
+        self.initUI()
 
-response = requests.get(geocoder_api_server, params=geocoder_params)
+    def getImage(self):
+        server_address = '[https://static-maps.yandex.ru/v1?](https://static-maps.yandex.ru/v1?)'
+        api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
+        ll_spn = f'll={x}%2C{y}&spn=0,69.00619'
+        map_request = f"{server_address}{ll_spn}&apikey={api_key}"
+        response = requests.get(map_request)
 
-if not response:
-    # обработка ошибочной ситуации
-    pass
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
 
-# Преобразуем ответ в json-объект
-json_response = response.json()
-# Получаем первый топоним из ответа геокодера.
-toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-# Координаты центра топонима:
-toponym_coodrinates = toponym["Point"]["pos"]
-# Долгота и широта:
-toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
+    def initUI(self):
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+        self.pixmap = QPixmap(self.map_file)
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
 
-# Собираем параметры для запроса к StaticMapsAPI:
-map_params = {
-    "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": find_delta(toponym),
-    "apikey": apikey,
-    "pt": f'{toponym_longitude},{toponym_lattitude},pm2dom37'
-}
+    def closeEvent(self, event):
+        os.remove(self.map_file)
 
-map_api_server = "https://static-maps.yandex.ru/v1"
-# ... и выполняем запрос
-response = requests.get(map_api_server, params=map_params)
-im = BytesIO(response.content)
-opened_image = Image.open(im)
-opened_image.show()  # Создадим картинку и тут же ее покажем встроенным просмотрщиком операционной системы
+
+if name == 'main':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
