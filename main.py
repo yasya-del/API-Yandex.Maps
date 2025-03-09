c5d15cc49d10985adf8df1bd123fd4d731c1bbf0
first.py <maxim.kuzmi4ev@gmail.com>
2025-03-09 11:30:02 +0300

    Большая задача №4

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import os
 import sys
-import math
 
 import pygame
 import requests
 
 
-def get_image(x1, y1):
+def get_image(theme):
     server_address = 'https://static-maps.yandex.ru/v1?'
     api_key = '1c61eb4c-5ad0-4d78-9b19-b45653666b96'
-    ll_spn = f'll={x1},{y1}&z={z}'
+    ll_spn = f'll={x},{y}&z={z}&theme={theme}'
     map_request = f"{server_address}{ll_spn}&apikey={api_key}"
     response = requests.get(map_request)
     if not response:
@@ -23,49 +22,29 @@ def get_image(x1, y1):
     return map_file
 
 
-LAT_STEP = 0.025  # Шаги при движении карты по широте и долготе
-LON_STEP = 0.01926
 x, y = map(float, input('Введите координаты объекта: ').split(','))
 z = int(input('Введите масштаб карты: '))
-map_file = get_image(x, y)
+zxc = 1
+map_file = get_image('dark')
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 FPS = 60
 clock = pygame.time.Clock()
-# Рисуем картинку, загружаемую из только что созданного файла.
 screen.blit(pygame.image.load(map_file), (0, 0))
-# Переключаем экран и ждем закрытия окна.
 pygame.display.flip()
 run = True
 while run:
     for event in pygame.event.get():
         if pygame.event.wait().type == pygame.QUIT:
             run = False
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEUP:
-            if z <= 20:
-                z += 1
-            map_file = get_image(x, y)
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_PAGEDOWN:
-            if z >= 1:
-                z -= 1
-            map_file = get_image(x, y)
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                get_image(x, y - LON_STEP * math.pow(2, 15 - z))
-                screen.blit(pygame.image.load(map_file), (0, 0))
-                pygame.display.flip()
-            if event.key == pygame.K_UP:
-                get_image(x, y + LON_STEP * math.pow(2, 15 - z))
-                screen.blit(pygame.image.load(map_file), (0, 0))
-                pygame.display.flip()
-            if event.key == pygame.K_LEFT:
-                get_image(x - LAT_STEP * math.pow(2, 15 - z), y)
-                screen.blit(pygame.image.load(map_file), (0, 0))
-                pygame.display.flip()
-            if event.key == pygame.K_RIGHT:
-                get_image(x + LAT_STEP * math.pow(2, 15 - z), y)
-                screen.blit(pygame.image.load(map_file), (0, 0))
-                pygame.display.flip()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            if zxc % 2 != 0:
+                theme = 'light'
+            elif zxc % 2 == 0:
+                theme = 'dark'
+            zxc += 1
+            map_file = get_image(theme)
+            # Переключение темы происходит тут, с помощью пробела
     screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
     clock.tick(FPS)
