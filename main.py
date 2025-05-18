cf1906bd8212079c792cd6549207ab144869020e
vasya <vasiliszaitseva@yandex.ru>
2025-05-18 15:41:38 +0300

    10 задача

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class Example(QMainWindow):
         self.scale = 1
         self.cur_type_map = 'map'
         self.setGeometry(100, 100, *self.SCREEN_SIZE)
-        self.setWindowTitle('Задание 9')
+        self.setWindowTitle('Задание 10')
         self.get_image(self.coords, self.scale)
 
         self.combobox = Combo(self)
@@ -53,10 +53,10 @@ class Example(QMainWindow):
         self.btn_combobox.resize(150, 30)
         self.btn_combobox.clicked.connect(self.btn_combobox_click)
 
-        self.seach_lineedit = QLineEdit(self)
-        self.seach_lineedit.setPlaceholderText('Введите место поиска здесь')
-        self.seach_lineedit.move(300, 465)
-        self.seach_lineedit.resize(190, 25)
+        self.search_lineedit = QLineEdit(self)
+        self.search_lineedit.setPlaceholderText('Введите место поиска здесь')
+        self.search_lineedit.move(300, 465)
+        self.search_lineedit.resize(190, 25)
 
         self.btn_lineedit = QPushButton('Начать поиск', self)
         self.btn_lineedit.move(300, 490)
@@ -75,154 +75,168 @@ class Example(QMainWindow):
         self.image = QLabel(self)
         self.image.resize(600, 450)
         self.image.setPixmap(self.pixmap)
+
         seach_params = {
             'geocode': 'Ярославль',
             'apikey': '40d1649f-0493-4b70-98ba-98533de7710b',
             'format': 'json'
         }
+
         link = '[https://geocode-maps.yandex.ru/1.x/](https://geocode-maps.yandex.ru/1.x/)'
         self.response = requests.get(link, seach_params)
 
         self.box_adresses = QCheckBox('почт. индекс', self)
         self.box_adresses.move(10, 460)
         self.box_adresses.clicked.connect(self.postal_code)
+
         self.is_postal_code = False
 
-        def postal_code(self):
-            self.is_postal_code = self.box_adresses.isChecked()
-
-        def btn_lineedit_click(self):
-            if not self.seach_lineedit.text():
-                return
-
-            seach_params = {
-                'geocode': self.seach_lineedit.text(),
-                'apikey': '40d1649f-0493-4b70-98ba-98533de7710b',
-                'format': 'json'
-            }
-
-            link = '[https://geocode-maps.yandex.ru/1.x/](https://geocode-maps.yandex.ru/1.x/)'
-            response = requests.get(link, seach_params)
-            self.response = response
-            check_response(response)
-
-            data = response.json()
+    def postal_code(self):
+        self.is_postal_code = self.box_adresses.isChecked()
+        data = self.response.json()
+        try:
+            coords = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty'][
+                'GeocoderMetaData']['text']
+        except Exception:
+            return
+        if self.is_postal_code:
             try:
-                coords = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point'][
-                    'pos'].split()
+                info = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty'][
+                    'GeocoderMetaData']['Address']['postal_code']
+                coords += f", {info}"
             except Exception:
                 return
-            coords = ','.join(coords)
+        self.search_lineedit.setText(coords)
 
-            if self.scale < 8:
-                self.scale = 8
-            elif self.scale > 12:
-                self.scale = 12
-            self.pt = f'{coords},pm2lbm'
-            self.get_image(coords, self.scale)
-            self.coords = coords
-            self.image.setPixmap(QPixmap(self.map_file))
+    def btn_lineedit_click(self):
+        if not self.search_lineedit.text():
+            return
+        seach_params = {
+            'geocode': self.search_lineedit.text(),
+            'apikey': '40d1649f-0493-4b70-98ba-98533de7710b',
+            'format': 'json'
+        }
 
-        def btn_addresses_clicked(self):
-            data = self.response.json()
-            try:
-                coords = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty'][
-                    'GeocoderMetaData']['text']
-                if self.is_postal_code:
-                    info = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty'][
-                        'GeocoderMetaData']['Address']['postal_code']
-                    coords += f", {info}"
-                self.seach_lineedit.setText(coords)
-            except Exception:
+        link = '[https://geocode-maps.yandex.ru/1.x/](https://geocode-maps.yandex.ru/1.x/)'
+        response = requests.get(link, seach_params)
+        self.response = response
+        check_response(response)
+
+        data = response.json()
+        try:
+            coords = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split()
+        except Exception:
+            return
+        coords = ','.join(coords)
+
+        if self.scale < 8:
+            self.scale = 8
+        elif self.scale > 12:
+            self.scale = 12
+        self.pt = f'{coords},pm2lbm'
+        self.get_image(coords, self.scale)
+        self.coords = coords
+        self.image.setPixmap(QPixmap(self.map_file))
+
+    def btn_addresses_clicked(self):
+        data = self.response.json()
+        try:
+            coords = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty'][
+                'GeocoderMetaData']['text']
+            if self.is_postal_code:
+                info = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty'][
+                    'GeocoderMetaData']['Address']['postal_code']
+                coords += f", {info}"
+            self.search_lineedit.setText(coords)
+        except Exception:
+            return
+
+    def btn_reset_click(self):
+        self.pt = ''
+        self.get_image(self.coords, self.scale)
+        self.image.setPixmap(QPixmap(self.map_file))
+
+    def btn_combobox_click(self):
+        translate = {'карта': 'map', 'спутник': 'sat', 'гибрид': 'sat,skl'}
+        self.cur_type_map = translate[self.combobox.currentText()]
+        self.get_image(self.coords, self.scale)
+        self.image.setPixmap(QPixmap(self.map_file))
+
+    def get_image(self, coords, scale):
+        coords = get_coords(scale, coords)
+
+        search_params = {
+            'll': coords,
+            'z': scale,
+            'l': self.cur_type_map
+        }
+        if self.pt != '':
+            search_params['pt'] = self.pt
+
+        link = '[http://static-maps.yandex.ru/1.x/](http://static-maps.yandex.ru/1.x/)'
+
+        response = requests.get(link, search_params)
+        check_response(response)
+        self.map_file = f"map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp and self.scale < 20:
+            self.scale += 1
+        elif event.key() == Qt.Key_PageDown and self.scale > 0:
+            self.scale -= 1
+
+        elif event.key() == Qt.Key_Left:
+            cords = self.coords.split(',')
+            step = 360 / pow(2, self.scale)
+            cords[0] = str(float(cords[0]) - abs(step))
+            if abs(float(cords[0])) >= 180:
                 return
+            self.coords = ','.join(cords)
 
-        def btn_reset_click(self):
-            self.pt = ''
-            self.get_image(self.coords, self.scale)
-            self.image.setPixmap(QPixmap(self.map_file))
-
-        def btn_combobox_click(self):
-            translate = {'карта': 'map', 'спутник': 'sat', 'гибрид': 'sat,skl'}
-            self.cur_type_map = translate[self.combobox.currentText()]
-            self.get_image(self.coords, self.scale)
-            self.image.setPixmap(QPixmap(self.map_file))
-
-        def get_image(self, coords, scale):
-            coords = get_coords(scale, coords)
-
-            search_params = {
-                'll': coords,
-                'z': scale,
-                'l': self.cur_type_map
-            }
-            if self.pt != '':
-                search_params['pt'] = self.pt
-
-            link = '[http://static-maps.yandex.ru/1.x/](http://static-maps.yandex.ru/1.x/)'
-
-            response = requests.get(link, search_params)
-            check_response(response)
-            self.map_file = f"map.png"
-            with open(self.map_file, "wb") as file:
-                file.write(response.content)
-
-        def keyPressEvent(self, event):
-            if event.key() == Qt.Key_PageUp and self.scale < 20:
-                self.scale += 1
-            elif event.key() == Qt.Key_PageDown and self.scale > 0:
-                self.scale -= 1
-
-            elif event.key() == Qt.Key_Left:
-                cords = self.coords.split(',')
-                step = 360 / pow(2, self.scale)
-                cords[0] = str(float(cords[0]) - abs(step))
-                if abs(float(cords[0])) >= 180:
-                    return
-                self.coords = ','.join(cords)
-
-            elif event.key() == Qt.Key_Right:
-                cords = self.coords.split(',')
-                step = 360 / pow(2, self.scale)
-                cords[0] = str(float(cords[0]) + abs(step))
-                if abs(float(cords[0])) >= 180:
-                    return
-                self.coords = ','.join(cords)
-
-            elif event.key() == Qt.Key_Up:
-                cords = self.coords.split(',')
-                step = 180 / pow(2, self.scale)
-                cords[1] = str(float(cords[1]) + abs(step))
-                if abs(float(cords[1])) >= 90:
-                    return
-                self.coords = ','.join(cords)
-            elif event.key() == Qt.Key_Down:
-                cords = self.coords.split(',')
-                step = 180 / pow(2, self.scale)
-                cords[1] = str(float(cords[1]) - abs(step))
-                if abs(float(cords[1])) >= 90:
-                    return
-                self.coords = ','.join(cords)
-            else:
+        elif event.key() == Qt.Key_Right:
+            cords = self.coords.split(',')
+            step = 360 / pow(2, self.scale)
+            cords[0] = str(float(cords[0]) + abs(step))
+            if abs(float(cords[0])) >= 180:
                 return
+            self.coords = ','.join(cords)
+        elif event.key() == Qt.Key_Up:
+            cords = self.coords.split(',')
+            step = 180 / pow(2, self.scale)
+            cords[1] = str(float(cords[1]) + abs(step))
+            if abs(float(cords[1])) >= 90:
+                return
+            self.coords = ','.join(cords)
 
-            self.get_image(self.coords, self.scale)
-            self.image.setPixmap(QPixmap('map.png'))
+        elif event.key() == Qt.Key_Down:
+            cords = self.coords.split(',')
+            step = 180 / pow(2, self.scale)
+            cords[1] = str(float(cords[1]) - abs(step))
+            if abs(float(cords[1])) >= 90:
+                return
+            self.coords = ','.join(cords)
+        else:
+            return
 
-        def closeEvent(self, event):
-            """При закрытии формы подчищаем за собой"""
-            os.remove('map.png')
+        self.get_image(self.coords, self.scale)
+        self.image.setPixmap(QPixmap('map.png'))
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove('map.png')
 
 class Combo(QComboBox):
-        def keyPressEvent(self, event):
-                ex.keyPressEvent(event)
+    def keyPressEvent(self, event):
+            ex.keyPressEvent(event)
 
-        def except_hook(cls, exception, traceback):
-            sys.excepthook(cls, exception, traceback)
-
+    def except_hook(cls, exception, traceback):
+        sys.excepthook(cls, exception, traceback)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    # sys.excepthook = except_hook
+    sys.excepthook = Combo().except_hook
     ex = Example()
     ex.show()
     sys.exit(app.exec())
